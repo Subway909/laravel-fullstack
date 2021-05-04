20bfdfc10647b44da3aab9b31cdd360308580763
henzo gomes <henzo.gomes@newm.com.br>
2021-05-04 19:40:15 -0300

    WIP frontend - listagem de usuários

--- sistema/src/views/Usuarios.vue
+++ sistema/src/views/Usuarios.vue
@@ -1,39 +1,144 @@
 <template>
   <v-container>
     <v-layout column class="card table">
-        <v-flex>Listagem de usuários</v-flex>
+      <v-flex mb-5>Listagem de usuários</v-flex>
+
+      <v-data-table
+        v-show="buscou"
+        :headers="headers"
+        :items="usuarios"
+        :items-per-page="5"
+        class="elevation-1"
+      >
+        <template v-slot:item.data_nascimento="{ item }">
+          {{ item.data_nascimento | moment }}
+        </template>
+
+        <template v-slot:item.btn="{ item }">
+          <v-btn @click="detalhes(item.id)">
+            Detalhes
+          </v-btn>
+        </template>
+      </v-data-table>
+
+      <v-row class="mt-10" v-show="exibirDetalhes">
+        <v-col>
+          <v-flex><h2>Listagem de usuários</h2></v-flex>
+
+          <v-card mt-5 style="padding: 10px;" color="#607d8b" dark>
+            <v-row wrap>
+
+              <v-col cols="6">
+                <h4>Dados cadastrais</h4>
+
+                <b>Nome:</b> {{ selectedUser.name }} <br>
+                <b>CPF:</b> {{ selectedUser.cpf }} <br>
+                <b>E-mail:</b> {{ selectedUser.email }} <br>
+                <b>Data de nascimento:</b> {{ selectedUser.data_nascimento | moment }} <br>
+              </v-col>
+              <v-col cols="6">
+                <h4>Telefone</h4>
+                <b>Telefone Fixo:</b> {{ selectedUserTelefone.telefone_fixo || '' }} <br>
+                <b>Telefone Celular:</b> {{ selectedUserTelefone.telefone_celular || '' }} <br>
+              </v-col>
+              <v-col cols="6">
+                <h4>Endereço</h4>
+
+                <b>Logradouro:</b> {{ selectedUserEndereco.logradouro}} <br>
+                <b>Número:</b> {{ selectedUserEndereco.numero}} <br>
+                <b>Bairro:</b> {{ selectedUserEndereco.bairro}} <br>
+                <b>Complemento:</b> {{ selectedUserEndereco.complemento}} <br>
+                <b>Cep:</b> {{ selectedUserEndereco.cep}} <br>
+                <b>Cidade:</b> {{ selectedUserEndereco.cidade}} <br>
+              </v-col>
+              <v-col cols="6">
+                <h4>Dados do certificado</h4>
+                <b>Nome:</b> {{ selectedUser.name }} <br>
+                <b>CPF:</b> {{ selectedUser.cpf }} <br>
+                <b>E-mail:</b> {{ selectedUser.email }} <br>
+                <b>Data de nascimento:</b> {{ selectedUser.data_nascimento | moment }} <br>
+              </v-col>
+            </v-row>
+
+          </v-card>
+        </v-col>
+      </v-row>
     </v-layout>
   </v-container>
 </template>
 
 <script>
 
-  export default {
-    name: "Usuarios",
-    data: () => ({
-      usuarios: [
-        {
-          id: "",
-          nome: "",
-          telefone: "",
-          email: "",
-          arquivo: "",
-        }
-      ]
-    }),
-    mounted() {
-      console.log('loaded usuarios')
-    },
-    methods: {
-      cors() {
-        this.$http.get('/teste').then((r) => {
-          console.log(r)
-        }).catch(err => {
-          alert("Verifique usuário e senha")
-          console.error(err)
-        })
+import moment from 'moment'
+import _ from 'lodash'
+
+export default {
+  name: "Usuarios",
+  data: () => ({
+    exibirDetalhes: false,
+    buscou: false,
+    selectedUser: [],
+    selectedUserTelefone: {},
+    selectedUserEndereco: {},
+    usuarios: [
+      {
+        id: "",
+        name: "",
+        cpf: "",
+        email: "",
+        data_nascimento: "",
       }
+    ],
+    headers: [
+      {
+        text: 'Nome',
+        align: 'start',
+        sortable: false,
+        value: 'name',
+      },
+      {text: 'CPF', value: 'cpf'},
+      {text: 'E-mail', value: 'email'},
+      {text: 'Data de nascimento', value: 'data_nascimento'},
+      {Text: '', value: 'btn'}
+    ]
+  }),
+  filters: {
+    moment: function (date) {
+      return moment(date).format('DD/MM/YYYY')
+    }
+  },
+  mounted() {
+    console.log('loaded usuarios')
+    console.log(this.usuarios)
+    this.listar()
+  },
+  methods: {
+    listar() {
+      this.$http.get('/index').then((r) => {
+        console.log(r)
+        this.usuarios = r.data
+        this.buscou = true
+      }).catch(err => {
+        console.error(err)
+      })
+    },
+    detalhes(id) {
+      this.exibirDetalhes = true
+
+      this.selectedUser = this.usuarios.find((s) => s.id === id)
+
+      this.selectedUserTelefone = _.find(this.usuarios, ['id', id]).telefones[0]
+      this.selectedUserEndereco = _.find(this.usuarios, ['id', id]).enderecos[0]
+
+      if (!this.selectedUserTelefone)
+        this.selectedUserTelefone = {}
+
+      if (!this.selectedUserEndereco)
+        this.selectedUserEndereco = {}
+
+      console.log(this.selectedUserEndereco)
     }
   }
+}
 
 </script>
